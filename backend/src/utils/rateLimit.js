ef05865341552ede875e5249dde2a71d2da0cac5
Pruthvi Parmar <ppruthviraj254@gmail.com>
2025-04-30 00:20:10 +0530

    email retry function added

--- backend/src/utils/rateLimit.js
+++ backend/src/utils/rateLimit.js
@@ -1,8 +1,19 @@
 import rateLimit from 'express-rate-limit'
 
-export const rateLimiter = rateLimit({
+export const generalRateLimiter = rateLimit({
     windowMs: 10 * 60 * 1000, // 10 minutes
     max: 30,                   // limit each IP to 30 requests per windowMs
+    message: {
+        success: false,
+        message: 'Too many API requests. Please try again later.'
+    },
+    standardHeaders: true, 
+    legacyHeaders: false, 
+});
+
+export const viewProfileRateLimiter = rateLimit({
+    windowMs: 10 * 60 * 100, // 1 minutes
+    max: 300,                   // limit each IP to 300 requests per windowMs
     message: {
         success: false,
         message: 'Too many API requests. Please try again later.'
